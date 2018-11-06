108b79a849a429160c41b2efb77240a368e750d5
Kurilo Denis <kdb2015@mail.ru>
2018-11-06 10:09:48 +0300

    commit photo insta

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){ 
 
+	var userFeed = new Instafeed({
+        get: 'user',
+        userId: '8435959384',
+        accessToken: '8435959384.1677ed0.fb3151ef0bfe4ee7b64ba742aba99074',
+        resolution : 'standard_resolution',
+        limit : 60
+    });
+    userFeed.run();
 
+    setTimeout(function(){
+    	$('.instafeed').addClass('shadowed');
+    }, 100);
 
 });
