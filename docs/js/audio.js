c66f3e68b731b6d803317b0da176c3a8ef7a0227
Kurilo Denis <kdb2015@mail.ru>
2018-11-06 11:12:20 +0300

    commit audio plauer

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
 
+	var audio = $('#audioTrack')[0];
+	$playBtn = $('#audio-play');
+	$pauseBtn = $('#audio-pause');
+
+	$audioControl = $('[data-type="audio-control]');
+
+	// initAudioPlayer();
+
+	setTimeout(initAudioPlayer, 800);
+
+	function initAudioPlayer(){
+		$playBtn.parent().show();
+		audio.loop = true;
+		audio.play();
+
+		$audioControl.click(function(){
+			if(audio.paused){
+				audio.play();
+				$playBtn.parent().hide();
+				$pauseBtn.parent().show();
+			}else{
+				audio.pause();
+				$playBtn.parent().show();
+				$pauseBtn.parent().hide();
+			}
+		});
+	}
 
 
 });
