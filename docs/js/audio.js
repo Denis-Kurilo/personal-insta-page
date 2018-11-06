16697f4ddab51f5978adb140cf4bb93c85c6980a
Kurilo Denis <kdb2015@mail.ru>
2018-11-06 11:36:22 +0300

    commit audio plauers

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,32 +1,27 @@
 $(document).ready(function(){
 
-	var audio = $('#audioTrack')[0];
-	$playBtn = $('#audio-play');
-	$pauseBtn = $('#audio-pause');
-
-	$audioControl = $('[data-type="audio-control]');
-
-	// initAudioPlayer();
-
-	setTimeout(initAudioPlayer, 800);
-
-	function initAudioPlayer(){
-		$playBtn.parent().show();
-		audio.loop = true;
-		audio.play();
-
-		$audioControl.click(function(){
-			if(audio.paused){
-				audio.play();
-				$playBtn.parent().hide();
-				$pauseBtn.parent().show();
-			}else{
-				audio.pause();
-				$playBtn.parent().show();
-				$pauseBtn.parent().hide();
-			}
-		});
-	}
-
+    var audio = $('#audioTrack')[0];
+    var $playBtn = $('#audio-play'),
+        $pauseBtn = $('#audio-pause'),
+        $audioControls = $('[data-type="audio-control"]');
+        
+    // initAudioPlayer();
+    setTimeout(initAudioPlayer, 800);
+    function initAudioPlayer() {
+        $playBtn.parent().show();
+        audio.loop = true;
+        audio.play();
+        $audioControls.on('click', function() {
+            if(audio.paused) {
+                audio.play();
+                $playBtn.parent().hide();
+                $pauseBtn.parent().show();
+            } else {
+                audio.pause();
+                $pauseBtn.parent().hide();
+                $playBtn.parent().show();
+            }
+        });
+    }
 
 });
